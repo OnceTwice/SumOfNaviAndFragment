bbd1eae32f2e340de0b3da9fa4b3024c949276e0
cho <you-smile@nate.com>
2017-01-30 22:43:50 +0900

    navigation click and make three fragment

--- app/src/main/java/com/example/cho/sumofnaviandfragment/MainActivity.java
+++ app/src/main/java/com/example/cho/sumofnaviandfragment/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.cho.sumofnaviandfragment;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,17 +13,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private Fragment loginFragment;
+    private Fragment joinFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginFragment = new LoginFragment();
+        joinFragment = new JoinFragment();
+
+        /**
+         * 기본 화면 설정
+         */
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.content_main, loginFragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+
+        // 1. Toolbar 객체 : 나중에 Toolbar를 애니메이션을 적용시켜서 부드러운 어플로 사용 가능
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // 2. FloatingActionButton 객체 : 우하단에 위치하고 있는 버튼
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -32,12 +49,15 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+        // 3. DrawerLayout 객체 : Drawer 기능을 이용하기 위해서 밑에 까는 레이아웃
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+
+        // 4. ActionBarDrawerToggle 객체 : ActionBar에서 좌상단에 위치한 버튼. NavigationView를 부를 때와 안부를 때 모양이 바뀌는 것을 하는 듯
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
 
+        // 5. NavigationView 객체 : 왼쪽에서 드래그해서 볼 수 있는 전광 view
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
     }
@@ -45,6 +65,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -77,13 +98,15 @@ public class MainActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            transaction.replace(R.id.content_main, loginFragment);
         } else if (id == R.id.nav_gallery) {
-
+            transaction.replace(R.id.content_main, joinFragment);
         } else if (id == R.id.nav_slideshow) {
 
         } else if (id == R.id.nav_manage) {
@@ -94,8 +117,12 @@ public class MainActivity extends AppCompatActivity
 
         }
 
+        transaction.addToBackStack(null);
+        transaction.commit();
+
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
+
         return true;
     }
 }
